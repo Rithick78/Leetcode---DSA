1585705e268f1c2777f0ae546882952823252b95
Rithick-dev <rithickjackson8@gmail.com>
2026-05-25 14:45:28 +0530

    Time: 0 ms (100%), Space: 53.1 MB (19.99%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -13,11 +13,11 @@ class Solution {
             if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1]){
                 return nums[mid];
             }
-            if((mid % 2 == 1 && nums[mid] == nums[mid+1]) || 
-               (mid % 2 == 0 && nums[mid] == nums[mid-1])){
-                   high = mid - 1;
+            if((mid % 2 == 1 && nums[mid] == nums[mid-1]) || 
+               (mid % 2 == 0 && nums[mid] == nums[mid+1])){
+                   low = mid + 1;
                } else {
-                 low = mid + 1;
+                 high = mid - 1;
                }
         }
         return -1;
