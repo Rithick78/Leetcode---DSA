19d9d22992bf2f088e2ad2d84e1a526dc798031a
Rithick-dev <rithickjackson8@gmail.com>
2026-06-01 02:56:50 +0530

    Time: 5 ms (33.93%), Space: 46.7 MB (38.04%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,22 +1,20 @@
 class Solution {
     public int romanToInt(String s) {
-        int res = 0;
-        Map<Character, Integer> roman = new HashMap<>();
-        roman.put('I', 1);
-        roman.put('V', 5);
-        roman.put('X', 10);
-        roman.put('L', 50);
-        roman.put('C', 100);
-        roman.put('D', 500);
-        roman.put('M', 1000);
+         Map<Character, Integer> mp = new HashMap<>();
+        mp.put('I', 1); mp.put('V', 5); mp.put('X', 10); mp.put('L', 50);
+        mp.put('C', 100); mp.put('D', 500); mp.put('M', 1000);
 
-        for (int i = 0; i < s.length() - 1; i++) {
-            if (roman.get(s.charAt(i)) < roman.get(s.charAt(i + 1))) {
-                res -= roman.get(s.charAt(i));
-            } else {
-                res += roman.get(s.charAt(i));
+        int total = 0;
+
+        for(int i = 0; i < s.length(); i++) {
+
+            if(i + 1 < s.length() && mp.get(s.charAt(i)) < mp.get(s.charAt(i+1))) {
+                total -= mp.get(s.charAt(i));
+            }
+            else {
+                total += mp.get(s.charAt(i));
             }
         }
-        return res + roman.get(s.charAt(s.length() - 1));        
+        return total;
     }
 }
