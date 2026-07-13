fe2f3554d1f5af3f428db26fa05b9c8cda347a6d
Rithick Jackson <rithickjackson8@gmail.com>
2026-07-13 19:28:10 +0530

    Time: 7 ms (64.81%), Space: 44.4 MB (47.9%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,55 +1,15 @@
 class Solution {
-      private void reverseString(StringBuilder s, int start, int end) {
-        while (start < end) {
-            char temp = s.charAt(start);
-            s.setCharAt(start, s.charAt(end));
-            s.setCharAt(end, temp);
-            start++;
-            end--;
-        }
-    }
-
     public String reverseWords(String s) {
-        int n = s.length();
+        String[] words = s.split("\\s+");
+        StringBuilder res = new StringBuilder();
 
-        StringBuilder sb = new StringBuilder(s);
-        reverseString(sb, 0, n - 1);
-
-        int i = 0, j = 0, start = 0, end = 0;
-
-        while (j < n) {
-
-            while (j < n && sb.charAt(j) == ' ') j++;
-            if (j == n) break;
-
-            start = i;
-
-            while (j < n && sb.charAt(j) != ' ') {
-                if (i < sb.length()) {
-                    sb.setCharAt(i++, sb.charAt(j++));
-                } else {
-                    sb.append(sb.charAt(j++));
-                    i++;
-                }
-            }
-
-            end = i - 1;
-
-            reverseString(sb, start, end);
-
-            if (j < n) {
-                if (i < sb.length()) {
-                    sb.setCharAt(i++, ' ');
-                } else {
-                    sb.append(' ');
-                    i++;
-                }
+        for (int i = words.length - 1; i >= 0; i--) {
+            res.append(words[i]);
+            if (i != 0) {
+                res.append(" ");
             }
         }
 
-        if (i > 0 && sb.charAt(i - 1) == ' ') i--;
-
-        return sb.substring(0, i);
+        return res.toString().trim();
     }
 }
-
