2a2ca0fe564963a3ce08b487697bb691ecfae34a
Rithick Jackson <rithickjackson8@gmail.com>
2026-07-21 08:17:25 +0530

    Time: 1 ms (26.69%), Space: 44.1 MB (16.03%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -0,0 +1,31 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+        int i = 0, j = 0;
+        List<Integer> list = new LinkedList<>();
+
+        while (i < m && j < n) {
+            if (nums1[i] <= nums2[j]) {
+                list.add(nums1[i]);
+                i++;
+            } else {
+                list.add(nums2[j]);
+                j++;
+            }
+        }
+
+        while (i < m) {
+            list.add(nums1[i]);
+            i++;
+        }
+
+        while (j < n) {
+            list.add(nums2[j]);
+            j++;
+        }
+
+        for (int k = 0; k < m + n; k++) {
+            nums1[k] = list.get(k);
+        }
+    }
+}
