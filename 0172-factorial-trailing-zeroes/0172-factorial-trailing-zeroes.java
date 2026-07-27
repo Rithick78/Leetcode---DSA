a832b4a00633a28f74b1667fc320f2c751e75e7b
Rithick Jackson <rithickjackson8@gmail.com>
2026-07-27 16:48:29 +0530

    Time: 0 ms (100%), Space: 42.4 MB (36.84%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.java
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.java
@@ -2,7 +2,7 @@ class Solution {
     public int trailingZeroes(int n) {
         int count = 0;
 
-        while (n > 0) {
+        while (n >= 5) {
             n /= 5;
             count += n;
         }
