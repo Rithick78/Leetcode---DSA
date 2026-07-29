15f33e0c83bb1d6f3cd93a1c795fe8381ae26f32
Rithick Jackson <rithickjackson8@gmail.com>
2026-07-29 17:11:50 +0530

    Time: 34 ms (56.36%), Space: 100 MB (49.31%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,28 +1,31 @@
 class MinStack {
-    private List<int[]> st;
+    Stack<Integer> st;
+    Stack<Integer> min;
 
     public MinStack() {
-        st = new ArrayList<>();
+        st = new Stack<>();
+        min = new Stack<>();
     }
-    
+
     public void push(int val) {
-        int[] top = st.isEmpty() ? new int[]{val, val} : st.get(st.size() - 1);
-        int min_val = top[1];
-        if (min_val > val) {
-            min_val = val;
+        if (min.isEmpty() || val <= min.peek()) {
+            min.push(val);
         }
-        st.add(new int[]{val, min_val});        
+        st.push(val);
     }
-    
+
     public void pop() {
-        st.remove(st.size() - 1);
+        if (st.peek().equals(min.peek())) {
+            min.pop();
+        }
+        st.pop();
     }
-    
+
     public int top() {
-        return st.isEmpty() ? -1 : st.get(st.size() - 1)[0];
+        return st.peek();
     }
-    
+
     public int getMin() {
-        return st.isEmpty() ? -1 : st.get(st.size() - 1)[1];
+        return min.peek();
     }
 }
