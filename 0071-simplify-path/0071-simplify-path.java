fd8c9591d95d48ba92c6c7c86467d9439ec75973
Rithick Jackson <rithickjackson8@gmail.com>
2026-07-29 16:02:10 +0530

    Time: 4 ms (96.31%), Space: 44.6 MB (87.74%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.java
+++ 0071-simplify-path/0071-simplify-path.java
@@ -1,33 +1,26 @@
 class Solution {
     public String simplifyPath(String path) {
-
-        String[] newPath = path.split("/");
+       String[] newPath = path.split("/");
         Stack<String> st = new Stack<>();
+        int n = newPath.length;
 
         for (String dir : newPath) {
+             if(dir.equals("") || dir.equals(".")) continue;
 
-            if (dir.equals("") || dir.equals(".")) {
-                continue;
-            }
-
-            if (dir.equals("..")) {
-                if (!st.isEmpty()) {
-                    st.pop();
-                }
-            } else {
+             if(dir.equals("..")) {
+                if(!st.isEmpty()) st.pop();
+             }else{
                 st.push(dir);
-            }
+             }
         }
 
-        if (st.isEmpty()) {
-            return "/";
-        }
+        if(st.isEmpty()) return "/";
 
         StringBuilder sb = new StringBuilder();
 
-        for (String dir : st) {
+        for(String s : st){
             sb.append("/");
-            sb.append(dir);
+            sb.append(s);
         }
 
         return sb.toString();
