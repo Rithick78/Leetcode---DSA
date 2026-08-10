659e0fb4b538b3fdc79a097151f6d07319c7d2d6
Rithick Jackson <rithickjackson8@gmail.com>
2026-08-10 16:33:45 +0530

    Time: 0 ms (100%), Space: 42.9 MB (79.16%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.java
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -9,17 +9,14 @@ class Solution {
             i = getNextValidIndex(s, i);
             j = getNextValidIndex(t, j);
 
-            // Both strings are completely processed
             if (i < 0 && j < 0) {
                 return true;
             }
 
-            // One string has characters remaining
             if (i < 0 || j < 0) {
                 return false;
             }
 
-            // Characters are different
             if (s.charAt(i) != t.charAt(j)) {
                 return false;
             }
