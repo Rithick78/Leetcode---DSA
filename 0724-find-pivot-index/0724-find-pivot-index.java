7f759e92982c5f0b44e658b2b346ee989b168581
Rithick Jackson <rithickjackson8@gmail.com>
2026-08-11 15:19:59 +0530

    Time: 1 ms (97.52%), Space: 47 MB (96.94%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,18 +1,19 @@
 class Solution {
     public int pivotIndex(int[] nums) {
         int total = 0;
-        for(int num : nums){
+        for (int num : nums) {
             total += num;
         }
-
-        int left = 0;
-        for(int i=0; i<nums.length;i++){
-            int right = total - left - nums[i];
-            if(right == left){
+        
+        int leftTotal = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int rightTotal = total - leftTotal - nums[i];
+            if (rightTotal == leftTotal) {
                 return i;
             }
-            left += nums[i];
+            leftTotal += nums[i];
         }
-        return -1;
+
+        return -1;        
     }
 }
