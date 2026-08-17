18ac4d80020b9b49515bbfcb6bc1ddbd869cf471
Rithick Jackson <rithickjackson8@gmail.com>
2026-08-17 16:30:29 +0530

    Time: 2 ms (99.34%), Space: 47.1 MB (41.11%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -4,14 +4,11 @@ class Solution {
         int n = nums.length;
 
         for(int i=0;i<n;i++){
-            int otherNum = target - nums[i];
-
-            if(mpp.containsKey(otherNum)){
-                return new int[]{mpp.get(otherNum), i};
+            int val = target - nums[i];
+            if(mpp.containsKey(val)){
+                return new int[] {mpp.get(val),i};
             }
-            
-            mpp.put(nums[i], i);
-
+            mpp.put(nums[i],i);
         }
         return new int[] {};
     }
