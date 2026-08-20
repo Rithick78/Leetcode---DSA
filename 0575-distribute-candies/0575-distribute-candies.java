74c508968ceeb4c6451da703e485fcdf589b5fde
Rithick Jackson <rithickjackson8@gmail.com>
2026-08-20 16:16:33 +0530

    Time: 30 ms (91.8%), Space: 47.9 MB (99.4%) - LeetHub

--- 0575-distribute-candies/0575-distribute-candies.java
+++ 0575-distribute-candies/0575-distribute-candies.java
@@ -1,15 +1,10 @@
 class Solution {
     public int distributeCandies(int[] c) {
-        Arrays.sort(c);
-        int max_candy = c.length/2;
-        int dif_candy = c[0];
-        int count = 1;
-        for(int i=1;i<c.length;i++){
-           if(dif_candy != c[i]){
-               dif_candy = c[i];
-               count++;
-           }
-        }
-        return count < max_candy ? count : max_candy;
+       int max_candy = c.length/2;
+       Set<Integer> set = new HashSet<>();
+       for(int num : c){
+           set.add(num);
+       }
+       return set.size() < max_candy ? set.size() : max_candy;
     }
 }
