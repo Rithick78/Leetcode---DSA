f29d7d37dbcc0e5c142c08443fc58933d02edb6f
Rithick Jackson <rithickjackson8@gmail.com>
2026-08-23 13:33:10 +0530

    Time: 6 ms (97.08%), Space: 46.8 MB (91.24%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean sumGame(String num) {
+        int n = num.length();
+
+        int sumLeft = 0, sumRight = 0;
+        int qLeft = 0, qRight = 0;
+
+        for (int i = 0; i < n / 2; i++) {
+            char c = num.charAt(i);
+            if (c == '?')
+                qLeft++;
+            else
+                sumLeft += c - '0';
+        }
+
+        for (int i = n / 2; i < n; i++) {
+            char c = num.charAt(i);
+            if (c == '?')
+                qRight++;
+            else
+                sumRight += c - '0';
+        }
+
+        if (((qLeft + qRight) & 1) == 1)
+            return true;
+
+        return sumLeft - sumRight != (qRight - qLeft) / 2 * 9;
+    }
+}
